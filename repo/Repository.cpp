f92c002b218634f9982cd03eac71a1ef6b292e95
AdelTamer <adeltammer@gmail.com>
2024-08-22 20:38:35 +0300

    Create Student, Course, Teacher Repos

---
+++ repo/Repository.cpp
@@ -0,0 +1,95 @@
+#include "../model/Model.cpp"
+using namespace std;
+
+// Abstract "Interface" Class For Student
+class StudentRepo
+{
+public:
+    virtual int addStudent(Student student) = 0;
+};
+// Concrete "Implementation" Class for Student Repository
+class StudentRepoImpl : public StudentRepo
+{
+private:
+    Student student[25];
+    int index = 0;
+
+public:
+    // override
+    int addStudent(Student student)
+    {
+        if (index == 25)
+        {
+            cout << "Cannot add more students. Repository is full." << endl;
+            return -1;
+        }
+        else
+        {
+            this->student[index] = student;
+            index++;
+            return 0;
+        }
+    }
+};
+
+// Abstract "Interface" Class For Course
+class CourseRepo
+{
+public:
+    virtual int addCourse(Course course) = 0;
+};
+// Concrete "Implementation" Class for Course Repository
+class CourseRepoImpl : public CourseRepo
+{
+private:
+    Course course[25];
+    int index = 0;
+
+public:
+    // override
+    int addCourse(Course course)
+    {
+        if (index == 25)
+        {
+            cout << "Cannot add more courses. Repository is full." << endl;
+            return -1;
+        }
+        else
+        {
+            this->course[index] = course;
+            index++;
+            return 0;
+        }
+    }
+};
+
+// Abstract "Interface" Class For Teacher
+class TeacherRepo
+{
+public:
+    virtual int addTeacher(Teacher teacher) = 0;
+};
+// Concrete "Implementation" Class for Teacher Repository
+class TeacherRepoImpl : public TeacherRepo
+{
+private:
+    Teacher teacher[25];
+    int index = 0;
+
+public:
+    // override
+    int addTeacher(Teacher teacher)
+    {
+        if (index == 25)
+        {
+            cout << "Cannot add more teachers. Repository is full." << endl;
+            return -1;
+        }
+        else
+        {
+            this->teacher[index] = teacher;
+            index++;
+            return 0;
+        }
+    }
+};
---
+++ services/Service.cpp
@@ -0,0 +1,62 @@
+#include "../repo/Repository.cpp"
+using namespace std;
+
+// Abstract "Interface" Class For Student
+class StudentService
+{
+public:
+    virtual int addStudent(Student student) = 0;
+};
+// Concrete "Implementation" Class for Student Service
+class StudentServiceImp : public StudentService
+{
+private:
+    StudentRepoImpl studentRepo;
+
+public:
+    // override
+    int addStudent(Student student)
+    {
+        return studentRepo.addStudent(student);
+    }
+};
+
+// Abstract "Interface" Class For Course
+class CourseService
+{
+public:
+    virtual int addCourse(Course course) = 0;
+};
+// Concrete "Implementation" Class for Course Service
+class CourseServiceImp : public CourseService
+{
+private:
+    CourseRepoImpl courseRepo;
+
+public:
+    // override
+    int addCourse(Course course)
+    {
+        return courseRepo.addCourse(course);
+    }
+};
+
+// Abstract "Interface" Class For Teacher
+class TeacherService
+{
+public:
+    virtual int addTeacher(Teacher teacher) = 0;
+};
+// Concrete "Implementation" Class for Teacher Service
+class TeacherServiceImp : public TeacherService
+{
+private:
+    TeacherRepoImpl teacherRepo;
+
+public:
+    // override
+    int addTeacher(Teacher teacher)
+    {
+        return teacherRepo.addTeacher(teacher);
+    }
+};
